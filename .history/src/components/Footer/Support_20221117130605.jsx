fdeeee66ad7b9a382d8ddaf1943a07a398b1b830
uh-sman <75017192+uh-sman@users.noreply.github.com>
2022-11-18 21:53:00 +0100

    responsive

---
+++ .history/src/components/Footer/Support_20221117130605.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import gr from "../assets/Group.png";
+import gr from "../assets/first.png";
+// import rect from "../assets/Rectangle44.png";
+import rect from "../assets/use.png";
+import "./support.css";
+const Support = () => {
+  return (
+    <div className="container">
+      <div className="shift">
+        <div style={{ color: "white" }}>
+          <h2>
+            Get all the support you need,
+            <br />
+            Contact our online agent
+          </h2>
+          <small className="down">
+            Get in touch or create account. We are 24/7 available
+          </small>
+          <img src={gr} className="top" />
+        </div>
+        <div className="button-section">
+          <button className="button">Contact Us</button>
+          <img src={rect} className="bottom" />
+        </div>
+        <div className="rectangle-container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
---
+++ .history/src/components/Section/SubCard_20221117122058.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import img1 from "../assets/Group.png";
+import img2 from "../assets/Rectangle44.png";
+import gr from "../assets/form.png";
+import first from "../assets/white.png";
+import group from "../assets/Group2.png";
+import "./subcard.css";
+const SubCard = () => {
+  return (
+    <div className="subcard-container">
+      <div className="subcard">
+        <div
+          className="cards"
+          style={{
+            backgroundColor: "#E4FAFF",
+            borderRadius: "15px",
+            height: "162px",
+          }}
+        >
+          <div className="texts">
+            <h4 style={{ color: "red", width: "412px" }}>Property Developer</h4>
+            <p>Information</p>
+            <button className="button-subcard" style={{ marginTop: "39px" }}>
+              Visit Developer Page
+            </button>
+            <img src={first} className="box2" />
+          </div>
+          <div className="box">
+            <img src={gr} className="box1" />
+          </div>
+        </div>
+        <div
+          className="cards"
+          style={{
+            backgroundColor: "#DEE1E2",
+            borderRadius: "15px",
+            height: "162px",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <div className="texts">
+              <h4 style={{ color: "red", width: "412px" }}>
+                Property Developer
+              </h4>
+              <p>Information</p>
+              <button className="button-subcard" style={{ marginTop: "39px" }}>
+                Visit Developer Page
+              </button>
+              <img src={first} className="box2" />
+            </div>
+            {/*  */}
+            <div className="box-gray"></div>
+          </div>
+        </div>
+        <div className="group">
+          <img src={group} className="group-image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubCard;
+// height: "162px";
